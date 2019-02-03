921c856ec2fce861f4a2b81eb0245aa9dc6f19b2
Leonie Koch <lk.leonie.koch@gmail.com>
2019-02-03 12:39:33 +0100

    Setting up homepage root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
+
   resources :adresses
   resources :roles
   resources :paying_methods
